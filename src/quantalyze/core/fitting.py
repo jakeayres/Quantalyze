e9eeed67889f238148f1f13a89946447110a2d9c
jakeayres <jake_ayres@hotmail.co.uk>
2025-03-20 23:22:17 +0000

    Enhance fitting and smoothing functions to support y-axis range filtering and improve parameter handling

--- src/quantalyze/core/fitting.py
+++ src/quantalyze/core/fitting.py
@@ -1,4 +1,5 @@
 from scipy.optimize import curve_fit
+from inspect import signature
 
 
 class Fit:
@@ -39,9 +40,10 @@ class Fit:
         ax.plot(x, self.evaluate(x), **kwargs)
 
 
-def fit(func, df, x_column, y_column, x_min=None, x_max=None):
+def fit(func, df, x_column, y_column, x_min=None, x_max=None, y_min=None, y_max=None, p0=None):
     """
-    Fits a given function to data in a DataFrame within an optional x range.
+    Fits a given function to data in a DataFrame within an optional x and y range.
+    
     Parameters:
     func (callable): The function to fit to the data. It should take x data as the first argument and parameters to fit as subsequent arguments.
     df (pandas.DataFrame): The input DataFrame containing the data.
@@ -49,10 +51,12 @@ def fit(func, df, x_column, y_column, x_min=None, x_max=None):
     y_column (str): The name of the column in the DataFrame to use as the y data.
     x_min (float, optional): The minimum value of x to include in the fitting. Defaults to None.
     x_max (float, optional): The maximum value of x to include in the fitting. Defaults to None.
+    y_min (float, optional): The minimum value of y to include in the fitting. Defaults to None.
+    y_max (float, optional): The maximum value of y to include in the fitting. Defaults to None.
+    p0 (array-like, optional): Initial guess for the parameters. Defaults to None.
+    
     Returns:
-    tuple: A tuple containing:
-        - popt (array): Optimal values for the parameters so that the sum of the squared residuals of `func(x_data, *popt) - y_data` is minimized.
-        - pcov (2-D array): The estimated covariance of popt. The diagonals provide the variance of the parameter estimate.
+    Fit: An instance of the Fit class containing the fitted function and parameters.
     """
     # Filter the dataframe based on x_min and x_max
     if x_min is not None:
@@ -60,15 +64,27 @@ def fit(func, df, x_column, y_column, x_min=None, x_max=None):
     if x_max is not None:
         df = df[df[x_column] <= x_max]
     
+    # Filter the dataframe based on y_min and y_max
+    if y_min is not None:
+        df = df[df[y_column] >= y_min]
+    if y_max is not None:
+        df = df[df[y_column] <= y_max]
+    
     # Extract x and y data
     x_data = df[x_column].values
     y_data = df[y_column].values
     
     # Check if there is data to fit
     if len(x_data) == 0 or len(y_data) == 0:
-        raise RuntimeError("No data in the specified x range to fit.")
+        raise RuntimeError("No data in the specified x or y range to fit.")
+    
+    # Check if p0 is provided and has the correct length
+    if p0 is not None:
+        num_params = len(signature(func).parameters) - 1  # Subtract 1 for the x parameter
+        if len(p0) != num_params:
+            raise ValueError(f"Initial guess p0 must have length {num_params}, but got {len(p0)}.")
     
     # Perform curve fitting
-    popt, pcov = curve_fit(func, x_data, y_data)
+    popt, pcov = curve_fit(func, x_data, y_data, p0=p0)
     
     return Fit(func=func, popt=popt, pcov=pcov)
--- src/quantalyze/core/smoothing.py
+++ src/quantalyze/core/smoothing.py
@@ -6,13 +6,13 @@ from scipy.signal import savgol_filter as scipy_savgol_filter
 
 
 
-def bin(dfs, x_column, minimum, maximum, width):
+def bin(dfs, column, minimum, maximum, width):
     """
     Bin the data in the specified column of the DataFrame(s) into equal-width bins and compute the mean of each bin.
 
     Parameters:
     dfs (pandas.DataFrame or list of pandas.DataFrame): The input DataFrame(s) containing the data to be binned.
-    x_column (str): The name of the column in the DataFrame(s) to be binned.
+    column (str): The name of the column in the DataFrame(s) to be binned.
     minimum (float): The minimum value of the range to be binned.
     maximum (float): The maximum value of the range to be binned.
     width (float): The width of each bin.
@@ -23,9 +23,9 @@ def bin(dfs, x_column, minimum, maximum, width):
     def bin_single_df(df):
         bin_edges = np.arange(minimum - width / 2, maximum + width / 2 + width, width)
         bin_midpoints = (bin_edges[1:] + bin_edges[:-1]) / 2
-        df['bin'] = pd.cut(df[x_column], bins=bin_edges, include_lowest=True)
+        df['bin'] = pd.cut(df[column], bins=bin_edges, include_lowest=True)
         binned = df.groupby('bin').agg('mean').reset_index()
-        binned[x_column] = bin_midpoints
+        binned[column] = bin_midpoints
         binned = binned.drop('bin', axis=1)
         return binned
 
@@ -37,37 +37,35 @@ def bin(dfs, x_column, minimum, maximum, width):
 
 
 
-def window(df, y_column, window_size=5, window_type='triang'):
+def window(df, column, window_size=5, window_type='triang'):
     """
     Apply a rolling window smoothing to a specified column in a DataFrame.
 
     Parameters:
     df (pandas.DataFrame): The input DataFrame containing the data.
-    y_column (str): The name of the column to which the rolling window smoothing will be applied.
+    column (str): The name of the column to which the rolling window smoothing will be applied.
     window_size (int, optional): The size of the rolling window. Default is 5.
     window_type (str, optional): The type of window to use. Default is 'triang'.
 
     Returns:
     pandas.Series: A Series with the smoothed values.
     """
-    return df[y_column].rolling(window=window_size, win_type=window_type, center=True).mean()
+    return df[column].rolling(window=window_size, win_type=window_type, center=True).mean()
 
 
-def savgol_filter(df, y_column, window_size=5, order=2):
+def savgol_filter(df, column, window_size=5, order=2):
     """
     Apply a Savitzky-Golay filter to a specified column in a DataFrame.
 
     Parameters:
     df (pandas.DataFrame): The input DataFrame containing the data.
-    y_column (str): The name of the column to which the Savitzky-Golay filter will be applied.
+    column (str): The name of the column to which the Savitzky-Golay filter will be applied.
     window_size (int, optional): The size of the window. Default is 5.
     order (int, optional): The order of the polynomial used to fit the samples. Default is 2.
 
     Returns:
     pandas.Series: A Series with the smoothed values.
     """
-    return pd.Series(scipy_savgol_filter(df[y_column], window_size, order), index=df.index)
-	
-	
+    return pd.Series(scipy_savgol_filter(df[column], window_size, order), index=df.index)
 	
 
