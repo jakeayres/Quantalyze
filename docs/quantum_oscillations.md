97bc3ae43b5b69fca7404d33d9f51ece6e520628
jakeayres <jake_ayres@hotmail.co.uk>
2025-07-13 13:38:37 +0100

    QO docs added

---
+++ docs/quantum_oscillations.md
@@ -0,0 +1,5 @@
+# Quantum Oscillations
+
+This module contains functions assisting in the analysis of quantum oscillation data.
+
+::: quantalyze.quantum_oscillations
--- src/quantalyze/quantum_oscillations.py
+++ src/quantalyze/quantum_oscillations.py
@@ -1,3 +1,9 @@
+"""
+This module provides functions for analyzing quantum oscillation data.
+It includes calculations for extremal areas and frequencies, damping factors (Lifshitz-Kosevich and Dingle),
+effective mass fitting, and Fast Fourier Transform analysis of quantum oscillation signals.
+"""
+
 from .core.constants import BOLTZMANN_CONSTANT, REDUCED_PLANCK_CONSTANT, ELEMENTARY_CHARGE, ELECTRON_MASS, PI
 from .core.fitting import fit
 from .core.interpolation import interpolate
@@ -6,44 +12,44 @@ from .core.fft import Window
 from numpy import sinh, exp, inf, linspace
 import pandas as pd
 
-def extremal_area(frequency):
+def extremal_area(frequency: float):
     """
     Calculate the extremal area from the frequency of quantum oscillations.
 
-    Parameters:
-    frequency (float): Frequency in Tesla.
+    Args:
+        frequency (float): Frequency in Tesla.
 
     Returns:
-    float: Extremal area in units of m^-2.
+        float: Extremal area in units of m^-2.
     """
     return frequency * (2 * PI * ELEMENTARY_CHARGE) / REDUCED_PLANCK_CONSTANT
 
 
-def extremal_frequency(area):
+def extremal_frequency(area: float):
     """
     Calculate the extremal frequency from the extremal area.
 
-    Parameters:
-    area (float): Extremal area in units of m^-2.
+    Args:
+        area (float): Extremal area in units of m^-2.
 
     Returns:
-    float: Frequency in Tesla.
+        float: Frequency in Tesla.
     """
     return (REDUCED_PLANCK_CONSTANT * area) / (2 * PI * ELEMENTARY_CHARGE)
 
 
-def lifshitz_kosevich_damping_factor(temperature, magnetic_field, effective_mass, harmonic=1):
+def lifshitz_kosevich_damping_factor(temperature: float, magnetic_field: float, effective_mass: float, harmonic: int = 1):
     """
     Calculate the Lifshitz-Kosevich damping factor for quantum oscillations.
 
-    Parameters:
-    temperature (float): Temperature in Kelvin.
-    magnetic_field (float): Magnetic field in Tesla.
-    effective_mass (float): Effective mass in units of the electron mass.
-    harmonic (int, optional): Harmonic number. Defaults to 1.
+    Args:
+        temperature (float): Temperature in Kelvin.
+        magnetic_field (float): Magnetic field in Tesla.
+        effective_mass (float): Effective mass in units of the electron mass.
+        harmonic (int, optional): Harmonic number. Defaults to 1.
 
     Returns:
-    float or np.ndarray: Amplitude of quantum oscillations.
+        float: Amplitude of quantum oscillations.
     """
     cyclotron_mass = effective_mass * ELECTRON_MASS
     x = (2 * PI**2 * BOLTZMANN_CONSTANT * cyclotron_mass * temperature * harmonic) / (REDUCED_PLANCK_CONSTANT * ELEMENTARY_CHARGE * magnetic_field)
@@ -51,76 +57,77 @@ def lifshitz_kosevich_damping_factor(temperature, magnetic_field, effective_mass
     return amplitude
 
 
-def dingle_lifetime(dingle_temperature):
+def dingle_lifetime(dingle_temperature: float):
     """
     Calculate the Dingle lifetime from the Dingle temperature.
 
-    Parameters:
-    dingle_temperature (float): Dingle temperature in Kelvin.
+    Args:
+        dingle_temperature (float): Dingle temperature in Kelvin.
 
     Returns:
-    float: Dingle lifetime in seconds.
+        float: Dingle lifetime in seconds.
     """
     return REDUCED_PLANCK_CONSTANT / (2 * PI * BOLTZMANN_CONSTANT * dingle_temperature)
 
 
-def dingle_temperature(dingle_lifetime):
+def dingle_temperature(dingle_lifetime: float):
     """
     Calculate the Dingle temperature from the Dingle lifetime.
 
-    Parameters:
-    dingle_lifetime (float): Dingle lifetime in seconds.
+    Args:
+        dingle_lifetime (float): Dingle lifetime in seconds.
 
     Returns:
-    float: Dingle temperature in Kelvin.
+        float: Dingle temperature in Kelvin.
     """
     return REDUCED_PLANCK_CONSTANT / (2 * PI * BOLTZMANN_CONSTANT * dingle_lifetime)
 
 
-def dingle_damping_factor(magnetic_field, effective_mass, lifetime, harmonic=1):
+def dingle_damping_factor(magnetic_field: float, effective_mass: float, lifetime: float, harmonic: int = 1):
     """
     Calculate the Dingle damping factor for quantum oscillations.
 
-    Parameters:
-    magnetic_field (float): Magnetic field in Tesla.
-    effective_mass (float): Effective mass in units of the electron mass.
-    lifetime (float): Dingle lifetime in seconds.
+    Args:
+        magnetic_field (float): Magnetic field in Tesla.
+        effective_mass (float): Effective mass in units of the electron mass.
+        lifetime (float): Dingle lifetime in seconds.
+        harmonic (int, optional): Harmonic number. Defaults to 1.
 
     Returns:
-    float: Dingle damping factor.
+        float: Dingle damping factor.
     """
     cyclotron_mass = effective_mass * ELECTRON_MASS
     cylcotron_frequency = ELEMENTARY_CHARGE * magnetic_field / cyclotron_mass
     return exp(- (PI * harmonic) / (cylcotron_frequency * lifetime))
 
 
-def mean_inverse_field(minimum, maximum):
+def mean_inverse_field(minimum: float, maximum: float):
     """
     Calculate the mean inverse field from the minimum and maximum fields.
 
-    Parameters:
-    minimum (float): Minimum magnetic field in Tesla.
-    maximum (float): Maximum magnetic field in Tesla.
+    Args:
+        minimum (float): Minimum magnetic field in Tesla.
+        maximum (float): Maximum magnetic field in Tesla.
 
     Returns:
-    float: Mean inverse field in Tesla^-1.
+        float: Mean inverse field in Tesla^-1.
     """
     return ((1 / minimum + 1 / maximum) / 2)
 
 
-def fit_effective_mass(df, temperature_column, amplitude_column, magnetic_field, harmonic=1):
+def fit_effective_mass(df: pd.DataFrame, temperature_column: str, amplitude_column: str, magnetic_field: float, harmonic: int = 1):
     """
     Fit the effective mass from quantum oscillation data.
 
-    Parameters:
-    df (pd.DataFrame): DataFrame containing quantum oscillation data.
-    temperature_column (str): Name of the column containing temperature data.
-    amplitude_column (str): Name of the column containing amplitude data.
-    magnetic_field (float): Magnetic field in Tesla.
-    harmonic (int, optional): Harmonic number. Defaults to 1.
+    Args:
+        df (pandas.DataFrame): DataFrame containing quantum oscillation data.
+        temperature_column (str): Name of the column containing temperature data.
+        amplitude_column (str): Name of the column containing amplitude data.
+        magnetic_field (float): Magnetic field in Tesla.
+        harmonic (int, optional): Harmonic number. Defaults to 1.
 
     Returns:
-    float: Effective mass in units of the electron mass.
+        float: Effective mass in units of the electron mass.
     """
 
     def model(temperature, prefactor, effective_mass):
@@ -151,18 +158,19 @@ def fft(
     """
     Perform a Fast Fourier Transform (FFT) on the quantum oscillation data.
 
-    Parameters:
-    df (pd.DataFrame): DataFrame containing quantum oscillation data.
-    field_column (str): Name of the column containing magnetic field data.
-    signal_column (str): Name of the column containing signal data.
-    minimum_field (float): Minimum magnetic field in Tesla.
-    maximum_field (float): Maximum magnetic field in Tesla.
-    points (int): Number of points for the FFT.
-    window (str): Window function to apply to the data.
-    background_function (callable): Function to model the background.
+    Args:
+        df (pandas.DataFrame): DataFrame containing quantum oscillation data.
+        field_column (str): Name of the column containing magnetic field data.
+        signal_column (str): Name of the column containing signal data.
+        minimum_field (float): Minimum magnetic field in Tesla.
+        maximum_field (float): Maximum magnetic field in Tesla.
+        points (int): Number of points for the FFT.
+        background_function (callable): Function to model the background.
+        window (Window, optional): Window function to apply to the data. Defaults to Window.HANN.
+        subtract_inverse_field (bool, optional): Whether to subtract the inverse field. Defaults to False.
 
     Returns:
-    pd.DataFrame: DataFrame containing the FFT results.
+        pandas.DataFrame: DataFrame containing the FFT results.
     """
     
     data = df.copy()
